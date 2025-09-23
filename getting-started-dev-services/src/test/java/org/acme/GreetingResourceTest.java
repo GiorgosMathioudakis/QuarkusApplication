bad42a1d3f0a3f942462d96c16bd0fd75b2f3039
Giorgos Mathioudakis <mathioudakes@gmail.com>
2025-09-23 12:56:09 +0300

    quarkus tuto app

--- getting-started-dev-services/src/test/java/org/acme/GreetingResourceTest.java
+++ getting-started-dev-services/src/test/java/org/acme/GreetingResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 class GreetingResourceTest {
@@ -14,7 +15,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+                .body(containsString("Hello"));
     }
 
 }
